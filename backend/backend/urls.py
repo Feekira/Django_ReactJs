0da5d83e8a089c5bd8d7333e770942b36d438928
feekira <felipe-ny1@hotmail.com>
2021-12-08 15:59:36 -0300

    Enable activate token and session

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,11 +2,16 @@ from django.urls import path,include
 from rest_framework import routers
 from .views import UserViewSet,GroupViewSet
 from django.contrib import admin
+from core.views import ListViewSet,ItemViewSet
 
+
+#Router endpoints
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'group', GroupViewSet)
+router.register(r'list', ListViewSet)
+router.register(r'item', ItemViewSet)
 
 
 urlpatterns = [
